0aa126b1157a2c3fb0f59692bfd60afbec9864f9
ShySame <67509397+ShySame@users.noreply.github.com>
2021-07-28 17:29:48 +0300

    parse
    def parse + 10 tests

---
+++ main.py
@@ -0,0 +1,34 @@
+import re
+
+
+def parse(query: str) -> dict:
+    dict = {}
+    key, znach = re.findall(r'(\w+)=', query), re.findall(r'=(\w+)', query)
+    for item1, item2 in zip(key, znach): dict.update({item1: item2})
+    print(dict)
+    return dict
+
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
+    #========================================================================================================================================
+    #-----------------------------------------------------------Dop_testi--------------------------------------------------------------------
+
+    assert parse('https://example.com/path/to/page?color=purple&?name=ferret&zhuk&') == {'color': 'purple','name': 'ferret'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=pur&qukareku') == {'name': 'ferret','color': 'pur'}
+    assert parse('http://example.com/name') == {}
+    assert parse('http://example.com/path/to/page?name=ferret&color=purple/lop=tut') == {'name': 'ferret','color': 'purple','lop': 'tut'}
+    assert parse('http://example.com/?name=Dima_Net') == {'name': 'Dima_Net'}
+    assert parse('https://example.com/path/to/page?s=s/d=d/f=f/color=purple&?name=ferret&zhuk&') == {'s': 's','d': 'd','f': 'f',
+                                                                                                     'color': 'purple','name': 'ferret'}
+    assert parse('https://example.com/path/to/page?name=ferret1983&color=pur&qukareku') == {'name': 'ferret1983','color': 'pur'}
+    assert parse('http://example.com/name=#=') == {}
+    assert parse('http://example.com/path/to/page?name=ferret&color=purple/lop=tut20_20') == {'name': 'ferret','color': 'purple','lop': 'tut20_20'}
+    assert parse('http://example.com/?name=Dima_Net&=&======') == {'name': 'Dima_Net'}
+
+
